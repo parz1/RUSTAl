e3a3c549014a0ae5f6e85ac0b2cabda4fd56368a
parz1 <1062134330@qq.com>
2019-03-20 20:47:46 +0800

    Cargo build guess_game

---
+++ guessgame/target/debug/deps/guessgame-ce24c15f11246209.d
@@ -0,0 +1,5 @@
+C:\Users\parzi\Documents\projects\rust\rusttest\guessgame\target\debug\deps\guessgame-ce24c15f11246209.exe: src\main.rs
+
+C:\Users\parzi\Documents\projects\rust\rusttest\guessgame\target\debug\deps\guessgame-ce24c15f11246209.d: src\main.rs
+
+src\main.rs:
---
+++ guessgame/target/debug/guessgame.d
@@ -0,0 +1,1 @@
+C:\Users\parzi\Documents\projects\rust\rusttest\guessgame\target\debug\guessgame.pdb: C:\Users\parzi\Documents\projects\rust\rusttest\guessgame\src\main.rs
